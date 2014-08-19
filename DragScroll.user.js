eaf4f27db453ceb21f184d5bac6eed7c94b521e7
Yury Shulaev <yury.shulaev@gmail.com>
2014-08-19 19:52:24 +0400

    Initial commit

---
+++ DragScroll.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name        DragScroll
+// @include     *
+// @version     1
+// @grant       none
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
+// ==/UserScript==
+
+(function ($) {
+	var dragging = false;
+	var initialLeft, initialTop, initialX, initialY;
+	var scrollable = $(window);
+
+	scrollable.contextmenu(function (e) {
+		return false;
+	});
+
+	scrollable.mousedown(function (e) {
+		if (e.which !== 3) {
+			return true;
+		}
+
+		dragging = true;
+		initialLeft = $(this).scrollLeft();
+		initialTop = $(this).scrollTop();
+		initialX = e.clientX;
+		initialY = e.clientY;
+
+		return false;
+	});
+
+	scrollable.mousemove(function (e) {
+		if (dragging) {
+			$(this).scrollLeft(initialLeft - (e.clientX - initialX));
+			$(this).scrollTop(initialTop - (e.clientY - initialY));
+		}
+
+		return false;
+	});
+
+	scrollable.mouseup(function () {
+		dragging = false;
+		return false;
+	});
+})(jQuery.noConflict(true));
